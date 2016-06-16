c5574e00f7feebfd5a97b32499768450ae15bda4
Luka Peschke <luka.peschke@epitech.eu>
2016-06-16 17:39:31 +0200

    Map import

--- src/export.lua
+++ src/export.lua
@@ -1,5 +1,5 @@
 function export_map(filename)
-    file = io.open(filename, "w+")
+    local file = io.open(filename, "w+")
     io.output(file)
     local header = {x = #(map[1]), y = #map}
     io.write(JSON:encode(header))
@@ -13,4 +13,45 @@ function export_map(filename)
     end
     io.output(io.stdout)
     io.close(file)
+end
+
+function import_map(filename)
+    local file = io.open(filename, "r")
+    if (file == nil) then return nil end
+
+    io.input(file)
+
+    local header = JSON:decode(io.read())
+    local map = {}
+    for y = 1, header.y, 1 do
+        local to_add = {}
+        for x = 1, header.x, 1 do
+            local tmp_table = JSON:decode(io.read())
+            if (tmp_table.type == "wall") then
+                table.insert(to_add, copy_table(blocks.wall))
+            elseif (tmp_table.type == "brick") then
+                table.insert(to_add, copy_table(blocks.brick))
+            elseif (tmp_table.type == "hole") then
+                table.insert(to_add, copy_table(blocks.hole))
+            elseif (tmp_table.type == "mud") then
+                table.insert(to_add, copy_table(blocks.mud))
+            elseif (tmp_table.type == "inflamable") then
+                table.insert(to_add, copy_table(blocks.inflamable))
+            elseif (tmp_table.type == "tnt") then
+                table.insert(to_add, copy_table(blocks.tnt))
+            elseif (tmp_table.type == "electric_box") then
+                table.insert(to_add, copy_table(blocks.electric_box))
+            elseif (tmp_table.type == "waterbomb") then
+                table.insert(to_add, copy_table(blocks.waterbomb))
+            elseif (tmp_table.type == "chest") then
+                table.insert(to_add, copy_table(blocks.chest))
+            else
+                table.insert(to_add, copy_table(blocks.floor))
+            end
+            to_add[x].state = tmp_table.state
+            to_add[x].frame = tmp_table.frame
+        end
+        table.insert(map, to_add)
+    end
+    return map
 end
